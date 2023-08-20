f575715b8519172f60b0791f42cae65e5952c26f
Bulat Kamalov <kamalovmarket@gmail.com>
2023-08-20 11:14:33 +0400

    Initial Commit

---
+++ Weather_MVVM_Aston/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_MVVM_Aston
+//
+//  Created by Bulat Kamalov on 20.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
